5b7896ffd4fd141cadd2e926a5520cfdd44bd784
Austin Cherry <chillingwithim@gmail.com>
2014-06-19 07:31:32 -0700

    docs & pic

--- skittles.go
+++ skittles.go
@@ -1,13 +1,25 @@
+// Miminal package for terminal colors/ANSI escape code.
+// Check out the source here https://github.com/acmacalister/skittles
+// Also see the example directory for another example on how to use skittles
+//
+// package main
+
+// import (
+//   "fmt"
+//   "github.com/acmacalister/skittles"
+// )
+
+// func main() {
+//   fmt.Println(skittles.Red("Red's my favorite color"))
+// }
 package skittles
 
 import (
 	"fmt"
 )
 
-type Attribute int
-
 const (
-	black Attribute = iota + 30
+	black = iota + 30
 	red
 	green
 	yellow
@@ -22,206 +34,206 @@ const (
 	inverse   = "7;"
 )
 
-func makeString(attr, text string, color Attribute) string {
+func makeString(attr, text string, color int) string {
 	return fmt.Sprintf("\033[%s%dm%s\033[0m", attr, color, text)
 }
 
-// black text \033[30m
+// make the terminal text black - \033[30m
 func Black(text string) string {
 	return makeString(regular, text, black)
 }
 
-// red text \033[30m
+// make the terminal text red - \033[30m
 func Red(text string) string {
 	return makeString(regular, text, red)
 }
 
-// green text \033[30m
+// make the terminal text green - \033[30m
 func Green(text string) string {
 	return makeString(regular, text, green)
 }
 
-// yellow text \033[30m
+// make the terminal text yellow - \033[30m
 func Yellow(text string) string {
 	return makeString(regular, text, yellow)
 }
 
-// blue text \033[30m
+//  make the terminal text blue - \033[30m
 func Blue(text string) string {
 	return makeString(regular, text, blue)
 }
 
-// magenta text \033[30m
+//  make the terminal text magenta - \033[30m
 func Magenta(text string) string {
 	return makeString(regular, text, magenta)
 }
 
-// cyan text \033[30m
+// make the terminal text cyan - \033[30m
 func Cyan(text string) string {
 	return makeString(regular, text, cyan)
 }
 
-// white text \033[30m
+// make the terminal text white - \033[30m
 func White(text string) string {
 	return makeString(regular, text, white)
 }
 
-// bold black text \033[1;30m
+// make the terminal text bold black - \033[1;30m
 func BoldBlack(text string) string {
 	return makeString(bold, text, black)
 }
 
-// bold red text \1;033[30m
+// make the terminal text bold red - \1;033[30m
 func BoldRed(text string) string {
 	return makeString(bold, text, red)
 }
 
-// bold green text \033[1;30m
+// make the terminal text bold green - \033[1;30m
 func BoldGreen(text string) string {
 	return makeString(bold, text, green)
 }
 
-// bold yellow text \033[1;30m
+// make the terminal text bold yellow - \033[1;30m
 func BoldYellow(text string) string {
 	return makeString(bold, text, yellow)
 }
 
-// bold blue text \033[1;30m
+// make the terminal text bold blue - \033[1;30m
 func BoldBlue(text string) string {
 	return makeString(bold, text, blue)
 }
 
-// bold magenta text \033[1;30m
+// make the terminal text bold magenta - \033[1;30m
 func BoldMagenta(text string) string {
 	return makeString(bold, text, magenta)
 }
 
-// bold cyan text \033[1;30m
+// make the terminal text bold cyan - \033[1;30m
 func BoldCyan(text string) string {
 	return makeString(bold, text, cyan)
 }
 
-// bold white text \033[1;30m
+// make the terminal text bold white - \033[1;30m
 func BoldWhite(text string) string {
 	return makeString(bold, text, white)
 }
 
-// blink black text \033[5;30m
+// make the terminal text blink black - \033[5;30m
 func BlinkBlack(text string) string {
 	return makeString(blink, text, black)
 }
 
-// blink red text \033[5;30m
+// make the terminal text blink red - \033[5;30m
 func BlinkRed(text string) string {
 	return makeString(blink, text, red)
 }
 
-// blink green text \033[5;30m
+// make the terminal text blink green - \033[5;30m
 func BlinkGreen(text string) string {
 	return makeString(blink, text, green)
 }
 
-// blink yellow text \033[5;30m
+// make the terminal text blink yellow - \033[5;30m
 func BlinkYellow(text string) string {
 	return makeString(blink, text, yellow)
 }
 
-// blink blue text \033[5;30m
+// make the terminal text blink blue - \033[5;30m
 func BlinkBlue(text string) string {
 	return makeString(blink, text, blue)
 }
 
-// blink magenta text \033[5;30m
+// make the terminal text blink magenta - \033[5;30m
 func BlinkMagenta(text string) string {
 	return makeString(blink, text, magenta)
 }
 
-// blink cyan text \033[5;30m
+// make the terminal text blink cyan - \033[5;30m
 func BlinkCyan(text string) string {
 	return makeString(blink, text, cyan)
 }
 
-// blink white text \033[5;30m
+// make the terminal text blink white - \033[5;30m
 func BlinkWhite(text string) string {
 	return makeString(blink, text, white)
 }
 
-// underline black text \033[4;30m
+// make the terminal text underline black - \033[4;30m
 func UnderlineBlack(text string) string {
 	return makeString(underline, text, black)
 }
 
-// underline red text \033[4;30m
+// make the terminal text underline red - \033[4;30m
 func UnderlineRed(text string) string {
 	return makeString(underline, text, red)
 }
 
-// underline green text \033[4;30m
+// make the terminal text underline green - \033[4;30m
 func UnderlineGreen(text string) string {
 	return makeString(underline, text, green)
 }
 
-// underline yellow text \033[4;30m
+// make the terminal text underline yellow - \033[4;30m
 func UnderlineYellow(text string) string {
 	return makeString(underline, text, yellow)
 }
 
-// underline blue text \033[4;30m
+// make the terminal text underline blue - \033[4;30m
 func UnderlineBlue(text string) string {
 	return makeString(underline, text, blue)
 }
 
-// underline magenta text \033[4;30m
+// make the terminal text underline magenta - \033[4;30m
 func UnderlineMagenta(text string) string {
 	return makeString(underline, text, magenta)
 }
 
-// underline cyan text \033[4;30m
+// make the terminal text underline cyan - \033[4;30m
 func UnderlineCyan(text string) string {
 	return makeString(underline, text, cyan)
 }
 
-// underline white text \033[4;30m
+// make the terminal text underline white - \033[4;30m
 func UnderlineWhite(text string) string {
 	return makeString(underline, text, white)
 }
 
-// inverse black text \033[7;30m
+// make the terminal text inverse black - \033[7;30m
 func InverseBlack(text string) string {
 	return makeString(inverse, text, black)
 }
 
-// inverse red text \033[7;30m
+// make the terminal text inverse red - \033[7;30m
 func InverseRed(text string) string {
 	return makeString(inverse, text, red)
 }
 
-// inverse green text \033[7;30m
+// make the terminal text inverse green - \033[7;30m
 func InverseGreen(text string) string {
 	return makeString(inverse, text, green)
 }
 
-// inverse yellow text \033[7;30m
+// make the terminal text inverse yellow - \033[7;30m
 func InverseYellow(text string) string {
 	return makeString(inverse, text, yellow)
 }
 
-// inverse blue text \033[7;30m
+// make the terminal text inverse blue - \033[7;30m
 func InverseBlue(text string) string {
 	return makeString(inverse, text, blue)
 }
 
-// inverse magenta text \033[7;30m
+// make the terminal text inverse magenta - \033[7;30m
 func InverseMagenta(text string) string {
 	return makeString(inverse, text, magenta)
 }
 
-// inverse cyan text \033[7;30m
+// make the terminal text inverse cyan - \033[7;30m
 func InverseCyan(text string) string {
 	return makeString(inverse, text, cyan)
 }
 
-// inverse white text \033[7;30m
+// make the terminal text inverse white - \033[7;30m
 func InverseWhite(text string) string {
 	return makeString(inverse, text, white)
 }
